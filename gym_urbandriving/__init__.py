057575221a8cc77e0c0f6bf6e9324ecaa39addff
Jerry Zhao <qwertyuiopghb@gmail.com>
2017-11-14 20:11:45 -0800

    Update __init__.py

--- gym_urbandriving/__init__.py
+++ gym_urbandriving/__init__.py
@@ -1,4 +1,4 @@
-from gym.envs.registration import register
+#from gym.envs.registration import register
 from gym_urbandriving.envs import UrbanDrivingEnv
 from gym_urbandriving.visualizer import PyGameVisualizer
 from gym_urbandriving.state import SimpleIntersectionState
